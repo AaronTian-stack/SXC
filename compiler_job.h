b438a32a9d7ff32be30c6d4a957829e8b5abd82f
Aaron Tian <84607804+AaronTian-stack@users.noreply.github.com>
2025-12-21 17:55:07 -0500

    Cleanup (#18)
    * add clang format
    
    * run format
    
    * Create .gitattributes
    
    * Update format-all.ps1
    
    * format sxc
    
    * use c types in calling code
    
    * remove include
    
    * const
    
    * add format check
    
    * rename images
    
    * Update build.yml
    
    * refactor ci
    
    * fix for linux runner
    
    * consistent formatter version

--- compiler_job.h
+++ compiler_job.h
@@ -10,66 +10,69 @@
 
 #include <oneapi/tbb/concurrent_vector.h>
 
-#include <qhenki/RHI/shader_compiler.h>
 #include <qhenki/RHI/shader.h>
+#include <qhenki/RHI/shader_compiler.h>
 
 namespace qhenki::sxc
 {
-	namespace fs = std::filesystem;
+namespace fs = std::filesystem;
 
-	struct CLIInput
-	{
-		std::string config_path;
-		std::string output_dir;
-		std::string_view pdb_dir;
-		std::span<const std::string> global_defines;
-		std::span<const std::string> include_paths;
-		gfx::ShaderModel shader_model;
-		CompilerInput::Optimization optimization;
-		bool debug_flag;
-	};
+struct CLIInput
+{
+    std::string config_path;
+    std::string output_dir;
+    std::string_view pdb_dir;
+    std::span<const std::string> global_defines;
+    std::span<const std::string> include_paths;
+    gfx::ShaderModel shader_model;
+    CompilerInput::Optimization optimization;
+    bool debug_flag;
+};
 
-	struct CompilerInputFile
-	{
-		CompilerInput input;
-		std::filesystem::path output_path;
-	};
+struct CompilerInputFile
+{
+    CompilerInput input;
+    std::filesystem::path output_path;
+};
 
-	struct OutputInfo
-	{
-		gfx::ShaderModel sm;
-		gfx::ShaderType st;
-		std::string_view entry_point;
-	};
+struct OutputInfo
+{
+    gfx::ShaderModel sm;
+    gfx::ShaderType st;
+    std::string_view entry_point;
+};
 
-	using CompilerInputVector = boost::container::small_vector<CompilerInput, 1>;
+using CompilerInputVector = boost::container::small_vector<CompilerInput, 1>;
 
-	class SXCJob
-	{
-		struct ConfigLineInput
-		{
-			std::optional<std::string> output_dir; // Overrides CLIInput output directory
-			std::string entry_point;
-			std::optional<std::vector<std::string>> defines; // list of range of possible range of values {}
-			std::optional<CompilerInput::Optimization> optimization; // Overrides CLIInput optimization
-			gfx::ShaderType shader_type;
-		};
+class SXCJob
+{
+    struct ConfigLineInput
+    {
+        std::optional<std::string> output_dir; // Overrides CLIInput output directory
+        std::string entry_point;
+        std::optional<std::vector<std::string>> defines;         // list of range of possible range of values {}
+        std::optional<CompilerInput::Optimization> optimization; // Overrides CLIInput optimization
+        gfx::ShaderType shader_type;
+    };
 
-		static gfx::ShaderType to_shader_type(const char* str);
-		static const char* shader_type_to_str(gfx::ShaderType type);
+    static gfx::ShaderType to_shader_type(const char* str);
+    static const char* shader_type_to_str(gfx::ShaderType type);
 
-	public:
+public:
+    static fs::path get_resolved_output_name(const OutputInfo& info,
+                                             const fs::path& input_path,
+                                             const std::string& output_dir,
+                                             size_t permutation_count);
+    static int parse_config(const CLIInput& input, tbb::concurrent_vector<CompilerInputVector>* compiler_inputs);
+};
 
-		static fs::path get_resolved_output_name(const OutputInfo& info, const fs::path& input_path, const std::string& output_dir, size_t permutation_count);
-		static int parse_config(const CLIInput& input, tbb::concurrent_vector<CompilerInputVector>* compiler_inputs);
-	};
+struct ShaderResultCount
+{
+    uint64_t succeeded_count;
+    uint64_t failed_count;
+    uint64_t skipped_count;
+};
 
-	struct ShaderResultCount
-	{
-		UINT64 succeeded_count;
-		UINT64 failed_count;
-		UINT64 skipped_count;
-	};
-
-	ShaderResultCount execute_compilation_job(tbb::concurrent_vector<CompilerInputVector>* inputs, const std::string& output_dir);
-}
+ShaderResultCount execute_compilation_job(tbb::concurrent_vector<CompilerInputVector>* inputs,
+                                          const std::string& output_dir);
+} // namespace qhenki::sxc
--- main.cpp
+++ main.cpp
@@ -1,180 +1,165 @@
-#include <argparse/argparse.hpp>
 #include <qhenki/RHI/shader_compiler.h>
-#include <magic_enum/magic_enum.hpp>
+#include <argparse/argparse.hpp>
 #include <filesystem>
+#include <magic_enum/magic_enum.hpp>
 #include "compiler_job.h"
 #include "graphics/d3d12/d3d12_shader_compiler.h"
 
 int main(int argc, char* argv[])
 {
-	argparse::ArgumentParser program("SXC", "0.1.0");
-	program.add_description("FXC/DXC batch shader compiler.");
-	program.set_prefix_chars("-+/");
-	program.set_assign_chars("=:");
+    argparse::ArgumentParser program("SXC", "0.1.0");
+    program.add_description("FXC/DXC batch shader compiler.");
+    program.set_prefix_chars("-+/");
+    program.set_assign_chars("=:");
 
-	{ // OPTIONAL ARGUMENTS
-		program
-			.add_argument("-pdb", "--pdb-path")
-			.help("pdb output path")
-			.nargs(1);
+    { // OPTIONAL ARGUMENTS
+        program.add_argument("-pdb", "--pdb-path").help("pdb output path").nargs(1);
 
-		program
-			.add_argument("-i", "--include-path")
-			.append()
-			.help("include path");
+        program.add_argument("-i", "--include-path").append().help("include path");
 
-		program
-			.add_argument("-g", "--global-defines")
-			.append()
-			.help("defines to be used for all shaders");
+        program.add_argument("-g", "--global-defines").append().help("defines to be used for all shaders");
 
-		program.add_argument("-dbg", "--debug-flag")
-			.flag()
-			.help("enable debug flag for all shaders");
+        program.add_argument("-dbg", "--debug-flag").flag().help("enable debug flag for all shaders");
 
-		program.add_argument("-o", "--optimization")
-			.choices("O0", "O1", "O2", "O3")
-			.nargs(1)
-			.default_value("O3")
-			.help("default optimization level for shaders");
-	}
+        program.add_argument("-o", "--optimization")
+            .choices("O0", "O1", "O2", "O3")
+            .nargs(1)
+            .default_value("O3")
+            .help("default optimization level for shaders");
+    }
 
-	std::string config_file_path;
+    std::string config_file_path;
 
-	{ // REQUIRED ARGUMENTS
-		program.add_group("Required arguments");
+    { // REQUIRED ARGUMENTS
+        program.add_group("Required arguments");
 
-		program
-			.add_argument("-c", "--config-path")
-			.help("config file path")
-			.nargs(1)
-			.store_into(config_file_path)
-			.required();
+        program.add_argument("-c", "--config-path")
+            .help("config file path")
+            .nargs(1)
+            .store_into(config_file_path)
+            .required();
 
-		program
-			.add_argument("-sm", "--shader-model")
-			.help("HLSL shader model [X_Y]")
-			.choices("5_0", "5_1", "6_0", "6_1", "6_2", "6_3", "6_4", "6_5", "6_6")
-			.nargs(1)
-			.required();
+        program.add_argument("-sm", "--shader-model")
+            .help("HLSL shader model [X_Y]")
+            .choices("5_0", "5_1", "6_0", "6_1", "6_2", "6_3", "6_4", "6_5", "6_6")
+            .nargs(1)
+            .required();
 
-		program
-			.add_argument("-out", "--output")
-			.nargs(1)
-			.help("output directory")
-			.required();
-	}
+        program.add_argument("-out", "--output").nargs(1).help("output directory").required();
+    }
 
-	try
-	{
-		program.parse_args(argc, argv);
+    try
+    {
+        program.parse_args(argc, argv);
 
-		// Verify config file path
-		if (!std::filesystem::exists(config_file_path))
-		{
-			throw std::runtime_error("Config file does not exist: " + program.get<std::string>("--config-path"));
-		}
+        // Verify config file path
+        if (!std::filesystem::exists(config_file_path))
+        {
+            throw std::runtime_error("Config file does not exist: " + program.get<std::string>("--config-path"));
+        }
 
-		// Set working directory to config file location
-		const auto config_dir = std::filesystem::path(config_file_path).parent_path();
-		if (!config_dir.empty())
-		{
-			std::filesystem::current_path(config_dir);
-		}
+        // Set working directory to config file location
+        const auto config_dir = std::filesystem::path(config_file_path).parent_path();
+        if (!config_dir.empty())
+        {
+            std::filesystem::current_path(config_dir);
+        }
 
-		const auto pdb = program.present<std::string>("--pdb-path");
-		const auto includes = program.present<std::vector<std::string>>("--include-path");
+        const auto pdb = program.present<std::string>("--pdb-path");
+        const auto includes = program.present<std::vector<std::string>>("--include-path");
 
-		{ // Verify optional arguments
+        { // Verify optional arguments
 
-			// Check PDB path exists if specified
-			if (pdb.has_value())
-			{
-				if (const auto& pdb_path = pdb.value(); !std::filesystem::exists(pdb_path))
-				{
-					throw std::runtime_error("Specified PDB path does not exist: " + std::string(pdb_path.begin(), pdb_path.end()));
-				}
-			}
+            // Check PDB path exists if specified
+            if (pdb.has_value())
+            {
+                if (const auto& pdb_path = pdb.value(); !std::filesystem::exists(pdb_path))
+                {
+                    throw std::runtime_error("Specified PDB path does not exist: " +
+                                             std::string(pdb_path.begin(), pdb_path.end()));
+                }
+            }
 
-			// Check if all include paths exist if specified
-			if (includes.has_value())
-			{
-				for (const auto& include_paths = includes.value(); const auto& include_path : include_paths)
-				{
-					if (!std::filesystem::exists(include_path))
-					{
-						throw std::runtime_error("Specified include path does not exist: " + include_path);
-					}
-				}
-			}
-		}
+            // Check if all include paths exist if specified
+            if (includes.has_value())
+            {
+                for (const auto& include_paths = includes.value(); const auto& include_path : include_paths)
+                {
+                    if (!std::filesystem::exists(include_path))
+                    {
+                        throw std::runtime_error("Specified include path does not exist: " + include_path);
+                    }
+                }
+            }
+        }
 
-		std::array<char, 7> sm_str;
-		if (snprintf(sm_str.data(), sm_str.size(), "SM_%s", program.get<std::string>("--shader-model").c_str()) < 0)
-		{
-			throw std::runtime_error("Failed to extract SM string");
-		}
+        std::array<char, 7> sm_str;
+        if (snprintf(sm_str.data(), sm_str.size(), "SM_%s", program.get<std::string>("--shader-model").c_str()) < 0)
+        {
+            throw std::runtime_error("Failed to extract SM string");
+        }
 
-		const auto sm = magic_enum::enum_cast<qhenki::gfx::ShaderModel>(sm_str.data());
-		if (!sm.has_value())
-		{
-			throw std::runtime_error("Failed to reflect shader model: " + std::string(sm_str.data()));
-		}
+        const auto sm = magic_enum::enum_cast<qhenki::gfx::ShaderModel>(sm_str.data());
+        if (!sm.has_value())
+        {
+            throw std::runtime_error("Failed to reflect shader model: " + std::string(sm_str.data()));
+        }
 
-		const auto optimization = magic_enum::enum_cast<CompilerInput::Optimization>(
-			program.get<std::string>("--optimization"));
-		if (!optimization.has_value())
-		{
-			throw std::runtime_error("Failed to reflect optimization");
-		}
+        const auto optimization = magic_enum::enum_cast<CompilerInput::Optimization>(
+            program.get<std::string>("--optimization"));
+        if (!optimization.has_value())
+        {
+            throw std::runtime_error("Failed to reflect optimization");
+        }
 
-		auto global_defines = program.present<std::vector<std::string>>("--global-defines");
-		qhenki::sxc::CLIInput input
-		{
-			.config_path = std::move(config_file_path),
-			.output_dir = program.get<std::string>("--output"),
-			.pdb_dir = pdb.has_value() ? pdb.value() : std::string_view{},
-			.global_defines = global_defines.has_value() ? global_defines.value() : std::span<const std::string>{},
-			.include_paths = includes.has_value() ? includes.value() : std::span<const std::string>{},
-			.shader_model = sm.value(),
-			.optimization = optimization.value(),
-			.debug_flag = program.get<bool>("--debug-flag")
-		};
+        auto global_defines = program.present<std::vector<std::string>>("--global-defines");
+        qhenki::sxc::CLIInput input{.config_path = std::move(config_file_path),
+                                    .output_dir = program.get<std::string>("--output"),
+                                    .pdb_dir = pdb.has_value() ? pdb.value() : std::string_view{},
+                                    .global_defines = global_defines.has_value() ? global_defines.value()
+                                                                                 : std::span<const std::string>{},
+                                    .include_paths = includes.has_value() ? includes.value()
+                                                                          : std::span<const std::string>{},
+                                    .shader_model = sm.value(),
+                                    .optimization = optimization.value(),
+                                    .debug_flag = program.get<bool>("--debug-flag")};
 
-		const auto start = std::chrono::steady_clock::now();
-		
-		tbb::concurrent_vector<qhenki::sxc::CompilerInputVector> inputs;
-		const auto num_lines = qhenki::sxc::SXCJob::parse_config(input, &inputs);
-		if (num_lines < 0)
-		{
-			fprintf(stderr, "Failed to parse config file: %s\n", input.config_path.c_str());
-			return 1;
-		}
+        const auto start = std::chrono::steady_clock::now();
 
-		constexpr auto buffer_length = 512;
-		std::array<char, buffer_length> buffer1, buffer2;
-		qhenki::gfx::D3D12ShaderCompiler::get_dll_path(buffer1.data(), buffer2.data(), buffer_length);
-		printf("Using shader compiler DLLs:\nDXC: %s\nFXC: %s\n", buffer1.data(), buffer2.data());
+        tbb::concurrent_vector<qhenki::sxc::CompilerInputVector> inputs;
+        const auto num_lines = qhenki::sxc::SXCJob::parse_config(input, &inputs);
+        if (num_lines < 0)
+        {
+            fprintf(stderr, "Failed to parse config file: %s\n", input.config_path.c_str());
+            return 1;
+        }
 
-		const auto result_count = qhenki::sxc::execute_compilation_job(&inputs, input.output_dir);
+        constexpr auto buffer_length = 512;
+        std::array<char, buffer_length> buffer1, buffer2;
+        qhenki::gfx::D3D12ShaderCompiler::get_dll_path(buffer1.data(), buffer2.data(), buffer_length);
+        printf("Using shader compiler DLLs:\nDXC: %s\nFXC: %s\n", buffer1.data(), buffer2.data());
 
-		const auto end = std::chrono::steady_clock::now();
+        const auto result_count = qhenki::sxc::execute_compilation_job(&inputs, input.output_dir);
 
-		printf("========== Build: %llu succeeded, %llu failed, %llu up-to-date ==========\n",
-			   result_count.succeeded_count, result_count.failed_count, result_count.skipped_count);
+        const auto end = std::chrono::steady_clock::now();
 
-		// Print duration in seconds with milliseconds
-		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		double seconds = ms / 1000.0;
+        printf("========== Build: %llu succeeded, %llu failed, %llu up-to-date ==========\n",
+               result_count.succeeded_count,
+               result_count.failed_count,
+               result_count.skipped_count);
 
-		printf("========== Build completed and took %.3f seconds ==========\n", seconds);
+        // Print duration in seconds with milliseconds
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        double seconds = ms / 1000.0;
 
-		return 0;
-	}
-	catch (const std::exception& err)
-	{
-		fprintf(stderr, "%s\n", err.what());
-		fprintf(stderr, "%s", program.help().str().c_str());
-		return 1;
-	}
+        printf("========== Build completed and took %.3f seconds ==========\n", seconds);
+
+        return 0;
+    }
+    catch (const std::exception& err)
+    {
+        fprintf(stderr, "%s\n", err.what());
+        fprintf(stderr, "%s", program.help().str().c_str());
+        return 1;
+    }
 }
